150ad29676207081ac5f6c77509c548cda0b2c7b
YagoErnandes <yago700@gmail.com>
2020-04-11 22:51:04 -0300

    aula 22 minuto 6

--- web/src/components/Notifications/index.js
+++ web/src/components/Notifications/index.js
@@ -1,33 +1,88 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import { MdNotifications } from 'react-icons/md'
+import { parseISO, formatDistance } from 'date-fns'
+import pt from 'date-fns/locale/pt'
 
-import { Container, Badge, NotificationList, Notification } from './styles'
+import api from '../../services/api'
+
+import {
+	Container,
+	Badge,
+	NotificationList,
+	Notification,
+	Scroll,
+} from './styles'
 
 export default function Notifications() {
+	const [visible, setVisible] = useState(false)
+	const [notifications, setNotifications] = useState([])
+
+	useEffect(() => {
+		async function loadNotifications() {
+			const response = await api.get('notifications')
+
+			const data = response.data.map((notification) => ({
+				...notification,
+				timeDistance: formatDistance(
+					parseISO(notification.createdAt),
+					new Date(),
+					{ addSuffix: true, locale: pt },
+				),
+			}))
+			setNotifications(data)
+		}
+		loadNotifications()
+	}, [])
+
+	function handleToggleVisible() {
+		setVisible((old) => !old)
+	}
+
 	return (
 		<Container>
-			<Badge hasUnread>
+			<Badge onClick={handleToggleVisible} hasUnread>
 				<MdNotifications color="#7159c1" size={20} />
 			</Badge>
 
-      <NotificationList>
-        <Notification unread>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
-        <Notification>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
-        <Notification>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
-      </NotificationList>
+			<NotificationList visible={visible}>
+				<Scroll>
+					<Notification unread>
+						<p>Você possui um novo agendamento para amanhã</p>
+						<time>há 2 dias</time>
+						<button type="button">Marcar como lida</button>
+					</Notification>
+					<Notification>
+						<p>Você possui um novo agendamento para amanhã</p>
+						<time>há 2 dias</time>
+						<button type="button">Marcar como lida</button>
+					</Notification>
+					<Notification>
+						<p>Você possui um novo agendamento para amanhã</p>
+						<time>há 2 dias</time>
+						<button type="button">Marcar como lida</button>
+					</Notification>
+					<Notification>
+						<p>Você possui um novo agendamento para amanhã</p>
+						<time>há 2 dias</time>
+						<button type="button">Marcar como lida</button>
+					</Notification>
+					<Notification>
+						<p>Você possui um novo agendamento para amanhã</p>
+						<time>há 2 dias</time>
+						<button type="button">Marcar como lida</button>
+					</Notification>
+					<Notification>
+						<p>Você possui um novo agendamento para amanhã</p>
+						<time>há 2 dias</time>
+						<button type="button">Marcar como lida</button>
+					</Notification>
+					<Notification>
+						<p>Você possui um novo agendamento para amanhã</p>
+						<time>há 2 dias</time>
+						<button type="button">Marcar como lida</button>
+					</Notification>
+				</Scroll>
+			</NotificationList>
 		</Container>
 	)
 }
